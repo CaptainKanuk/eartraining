cc6560e7394bf2feb5474257e2595ac54958b3ad
Delaney Granizo-Mackenzie <dgranizo@nat-oitwireless-inside-vapornet100-a-10794.princeton.edu>
2014-04-18 01:43:22 -0400

    Added melody page

--- pages/views.py
+++ pages/views.py
@@ -29,6 +29,10 @@ def intervals(request):
 	context=RequestContext(request)
 	return render_to_response('pages/intervals.html', context)
 
+def melodies(request):
+	context=RequestContext(request)
+	return render_to_response('pages/melodies.html', context)
+
 def about(request):
 	context=RequestContext(request)
 	return render_to_response('pages/about.html', context)
@@ -100,13 +104,21 @@ def register_user(request):
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/register_success/')
+        else:
+            return register_failure(request)
 
-#    args = {}
-    #   args.update(csrf(request))
+    args = {}
+    args.update(csrf(request))
 
-#args['form'] = UserCreationForm()
+    args['form'] = UserCreationForm()
     return render_to_response('pages/404.html',context)
 
 #Success page on registering a user
 def register_success(request):
     return render_to_response('pages/register_success.html')
+
+#Failure page with tips on how to fix it
+def register_failure(request):
+    args = {}
+    args['errors'] = UserCreationForm(request.POST).errors
+    return render_to_response('pages/register_failure.html', args)
