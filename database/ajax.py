673b52d444bb557060a60c15c55ba66cb4a9ac9e
Michael Mulshine <michaelmulshine@nat-oitwireless-inside-vapornet100-a-10163.princeton.edu>
2014-05-02 01:17:09 -0400

    Adding for db practice.

--- database/ajax.py
+++ database/ajax.py
@@ -1,6 +1,32 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
 
-@dajaxice_register
-def sayhello(request):
-    return simplejson.dumps({'message':'Hello World'})
+
+@dajaxice_register(method='POST', name='user_level.update')
+@dajaxice_register(method='GET', name='user_level.info')
+def user_level(request):
+	# return simplejson.dumps({'message':'hello'})
+	if request.method == 'POST':
+		return simplejson.dumps({'top_level': level})
+	else:
+		return simplejson.loads({'top_level'})
+
+
+# @dajaxice_register
+# def sayhello(request):
+#     return simplejson.dumps({'message':'Hello World'})
+
+
+
+
+
+
+# @dajaxice_register
+# def sayhello(request):
+#     return simplejson.dumps({'message':'Hello World'})
+# @dajaxice_register
+# def setlevel(request):
+# 	return simplejson.dumps({'top_level': 2})
+# @dajaxice_register(method= "GET")
+# def getlevel(request):
+# 	return simplejson.dumps({'top_level': 2})
--- static/js/ajax-functions.js
+++ static/js/ajax-functions.js
@@ -2,3 +2,14 @@ function my_js_callback(data){
     alert('Got here!');
     alert(data.message);
 }
+
+function level_callback(data){
+	alert('Got here!');
+	alert(data.message);
+}
+
+function test_function()
+{
+	Dajaxice.database.sayhello(level_callback);
+}
+//callback functions that takes data 
