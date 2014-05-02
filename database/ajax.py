0a65ddd581e617b5398e9a63770518f2a61186f2
Delaney Granizo-Mackenzie <dgranizo@nat-oitwireless-inside-vapornet100-a-10794.princeton.edu>
2014-05-02 02:11:57 -0400

    Committing before pull.

--- database/ajax.py
+++ database/ajax.py
@@ -1,5 +1,6 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
+from pages.models import UserProfile
 
 @dajaxice_register
 def sayhello(request):
--- static/js/ajax-functions.js
+++ static/js/ajax-functions.js
@@ -1,4 +1,3 @@
 function my_js_callback(data){
-    alert('Got here!');
     alert(data.message);
 }
