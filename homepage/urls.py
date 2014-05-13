750f4b18253ec91d46999b89e16ea7db174886ba
bananaren <aren@princeton.edu>
2014-05-12 22:07:46 -0400

    yus

--- homepage/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from homepage import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^about/$', views.about, name='about'))
